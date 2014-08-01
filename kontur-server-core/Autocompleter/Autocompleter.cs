8ff6cafb8dcf9b67170e40a47d056e234aa61750
kirillsulim <kirillsulim@gmail.com>
2014-08-01 10:51:37 +0600

    Autocompleter - cache added

--- kontur-server-core/Autocompleter/Autocompleter.cs
+++ kontur-server-core/Autocompleter/Autocompleter.cs
@@ -14,6 +14,9 @@ namespace kontur_server_core
     {
         private Dictionary<string, int> d;
 
+        private Dictionary<string, string[]> cache1Letter;
+        private Dictionary<string, string[]> cache2Letters;
+
         /// <summary>
         /// Get dictionary using getter
         /// </summary>
@@ -21,15 +24,41 @@ namespace kontur_server_core
         public Autocompleter(IDictionaryGetter getter)
         {
             d = new Dictionary<string, int>(getter.Get());
+
+            cache1Letter = new Dictionary<string, string[]>();
+            cache2Letters = new Dictionary<string, string[]>();
         }
 
         public string[] Get(string index)
         {
-            // TODO: Add optimization if have some time
+            if (index.Length == 1)
+                return GetFrom1Cache(index);
+            if (index.Length == 2)
+                return GetFrom2Cache(index);
+
+            return GetFromDictionary(index);
+        }
+
+        private string[] GetFrom1Cache(string index)
+        {
+            if (!cache1Letter.ContainsKey(index))
+                cache1Letter[index] = GetFromDictionary(index);
+            return cache1Letter[index];
+        }
+
+        private string[] GetFrom2Cache(string index)
+        {
+            if (!cache2Letters.ContainsKey(index))
+                cache2Letters[index] = GetFromDictionary(index);
+            return cache2Letters[index];
+        }
+
+        private string[] GetFromDictionary(string index)
+        {
             var res = (from x in d
-                      where x.Key.StartsWith(index)
-                      orderby x.Value descending, x.Key
-                      select x.Key).Take(10);
+                       where x.Key.StartsWith(index)
+                       orderby x.Value descending, x.Key
+                       select x.Key).Take(10);
 
             return res.ToArray();
         }
